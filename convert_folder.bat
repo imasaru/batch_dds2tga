a203c3c387116b851124a4d28a95a331e60ee3f3
imasaru <evanmr4@gmail.com>
2017-07-06 15:36:03 -0500

    Fixed clerical mistakes
    I hope that you are not offended by my doing this >_< I just want to make it better.

--- convert_folder.bat
+++ convert_folder.bat
@@ -6,13 +6,13 @@ set destdir=%~dp1%~nx1_converted\
 
 echo %srcdir%
 echo %destdir%
-set /p ok="Is them ok? (Y/N)"
+set /p ok="Is this OK? (Y/N)"
 if !ok!==N exit
 if !ok!==n exit
 
-set /p del="Delete copied dds files after convert? (Y/N)"
+set /p del="Delete copied .dds files after conversion? (Y/N)"
 
-set /p rec="Make the conversion recursively? (Y/N)"
+set /p rec="Perform the conversion recursively? (Y/N)"
 
 set dorec=false
 if !rec!==y set dorec=true
@@ -25,14 +25,14 @@ rem copy the dds files only one folder deep
 if !dorec!==false xcopy "%srcdir%*.dds" "%destdir%" /c /r /d /y /i /q
 
 rem flush log
-break > %~dp0\convert_log.txt
+break > %~dp0\conversion_log.txt
 
 rem convert the dds files to tga
-for /r %destdir% %%i in (*.dds) do ( 
+for /r %destdir% %%i in (*.dds) do (
 	set file=%%~ni
 	echo [!TIME!] Converting: !file!
 	%~dp0\readdxt.exe "%%i"
-	echo [!TIME!] Converted: %%i >> %~dp0\convert_log.txt
+	echo [!TIME!] Converted: %%i >> %~dp0\conversion_log.txt
 )
 
 for /r %destdir% %%i in (*.tga) do (
@@ -40,10 +40,10 @@ for /r %destdir% %%i in (*.tga) do (
     set path=%%~dpi
     set tmp=%%~nxi
     set nn=%%~ni
-	rem delete unwanted tga files (*01.tga, *02.tga...) 
+	rem delete unwanted tga files (*01.tga, *02.tga...)
     if not !tmp:~-6!==00.tga (
-		del %%i 
-		echo [!TIME!] Deleted: !tmp! >> %~dp0\convert_log.txt
+		del %%i
+		echo [!TIME!] Deleted: !tmp! >> %~dp0\conversion_log.txt
 	)
     if !tmp:~-6!==00.tga (
 		rem rename converted tga to match the dds (remove 00 postfix)
@@ -53,13 +53,13 @@ for /r %destdir% %%i in (*.tga) do (
 		if !del!==Y del !path!!nn:~0,-2!.dds
 
 		echo [!TIME!] Clean up: !nn:~0,-2!.tga
-		echo [!TIME!] Clean up: !path!!nn:~0,-2!.tga >> %~dp0\convert_log.txt
+		echo [!TIME!] Clean up: !path!!nn:~0,-2!.tga >> %~dp0\conversion_log.txt
     )
-	
+
 )
 
-echo [!TIME!] Convert process READY >> %~dp0\convert_log.txt
-echo [!TIME!] Convert process READY
+echo [!TIME!] Conversion process READY >> %~dp0\conversion_log.txt
+echo [!TIME!] Conversion process READY
 
 pause
 exit
